ea66d015b394f3605396188f1196dcdf4f21aa6b
Matheus Aquino de Andrade <teteuaquinao01@hotmail.com>
2025-05-15 12:01:34 -0300

    Lista_de_Tarefas2.0

--- To-lis/js/script.js
+++ To-lis/js/script.js
@@ -1,74 +1,172 @@
 let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
-
-tarefas.forEach(function(tarefa) {
-    criarItem(tarefa);
+tarefas.forEach(novaTarefa => {
+    criarItem(novaTarefa);
 });
 
-console.log(tarefas);
+
+
+let filtro = document.getElementById('filtrar');
+
+filtro.addEventListener('change', function () {
+    filtrarTarefas(filtro.value);
+});
+
+
+let filterCate = document.getElementById('filtercate');
+
+filterCate.addEventListener('change', function () {
+    filtrarCategoria(filterCate.value);
+});
+
+
+//--------------------------------------------------------------------------------------------------------------------//
+// Função para Adicionar a Tarefa//
 function adicionar() {
     let input = document.getElementById('tarefa');
-    let tarefa = input.value.trim();  
-    if (tarefa === '') return; 
+    let date = document.getElementById('data');
+    let select = document.getElementById('categorias');
 
-    tarefas.push(tarefa); 
+    let tarefa = input.value.trim();
+    let tarefaData = date.value;
+    let categoria = select.value;
+
+    if (tarefa === '' || tarefaData === '' || categoria === '') return;
+    
+    let novaTarefa = {
+        texto: tarefa,
+        data: tarefaData,
+        categorias: categoria,
+        concluida: false
+    };
+    
+    tarefas.push(novaTarefa); 
     localStorage.setItem('tarefas', JSON.stringify(tarefas)); 
 
-    criarItem(tarefa); 
+    criarItem(novaTarefa); 
     input.value = ''; 
+    date.value = '';
+    categoria.value = '';
 }
-
-function criarItem(tarefa) {
+//-------------------------------------------------------------------------------------------------------------------//
+// Função para Criar a Tarefa//
+function criarItem(novaTarefa) {
     let lista = document.getElementById('lista');
     let item = document.createElement('li');
 
     let span = document.createElement('span');
-    span.innerText = tarefa;
+    span.innerText = novaTarefa.texto;
 
-    let buttom = document.createElement('button');
-    buttom.innerText = 'x';
-    buttom.classList.add('buttom');
+    let spanData = document.createElement('span');
+    let partes = novaTarefa.data.split('-'); // separa em [AAAA, MM, DD]
+    let dataFormatada = `${partes[2]}/${partes[1]}/${partes[0]}`;
+    spanData.innerText = `Data: ${dataFormatada}`;
+    spanData.style.marginLeft = '10px';
+    spanData.style.fontSize = '12px';
+    spanData.style.color = 'gray';
+
+    let spanCategorio = document.createElement('span');
+    spanCategorio.innerHTML = `Categoria: ${novaTarefa.categorias}`;
+
+    let button = document.createElement('button');
+    button.innerText = 'x';
+    button.classList.add('button');
 
     let btnEdt = document.createElement('button');
     btnEdt.innerText = 'Editar';
 
-    item.appendChild(span);
-    item.appendChild(buttom);
-    item.appendChild(btnEdt);
-    lista.appendChild(item);
-    
+    let btnConcluida = document.createElement('button');
+    btnConcluida.innerText = 'Concluido';
 
-    item.addEventListener('click', function(e) {
-        if (e.target !== buttom) {
-            item.classList.toggle('concluida');
-        }
+    if (novaTarefa.concluida) {
+        item.classList.add('concluida');
+    }
+    
+    item.appendChild(span);
+    item.appendChild(spanData);
+    item.appendChild(spanCategorio);
+    item.appendChild(btnConcluida);
+    item.appendChild(btnEdt);
+    item.appendChild(button);
+    lista.appendChild(item);
+
+    btnConcluida.addEventListener('click', function(e) {
+       item.classList.toggle('concluida');
+
+    // Atualiza a tarefa no array
+        let index = tarefas.findIndex(t => 
+            t.texto === novaTarefa.texto && 
+            t.data === novaTarefa.data && 
+            t.categorias === novaTarefa.categorias
+    )   ;
+
+        if (index !== -1) {
+            tarefas[index].concluida = item.classList.contains('concluida');
+            localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    }
+        
     });
 
     btnEdt.addEventListener('click', function(e) {
         e.stopPropagation();
-        if (e.target !== buttom) {
+        if (e.target !== button) {
             let textoAntigo = span.innerText;
-             let editor = prompt('Editar tarefa', textoAntigo);
+            let editor = prompt('Editar tarefa', textoAntigo);
              if(editor){
                 span.innerText = editor
-                let editorIndici = tarefas.indexOf(textoAntigo);
+                let editorIndici = tarefas.findIndex(t => t.texto === textoAntigo);
                 if (editorIndici !== -1){
-                    tarefas[editorIndici] = editor;
-
+                    tarefas[editorIndici].texto = editor;
                 }
                 localStorage.setItem('tarefas', JSON.stringify(tarefas));
              }
         }
     });
 
-    buttom.addEventListener('click', function(e) {
+    button.addEventListener('click', function(e) {
         e.stopPropagation();
-        tarefas = tarefas.filter(t => t !== tarefa);
+        tarefas = tarefas.filter(t => t !== novaTarefa);
         localStorage.setItem('tarefas', JSON.stringify(tarefas));
         item.remove();
     });
 }
+//-------------------------------------------------------------------------------------------------------------------//
+function filtrarTarefas(filtroSelecionado) {
+    const itens = document.querySelectorAll('#lista li');
 
+    itens.forEach(item => {
+        const estaConcluida = item.classList.contains('concluida');
+        
+
+        if (filtroSelecionado === 'todos') {
+            item.style.display = 'flex';
+        } else if (filtroSelecionado === 'concluido') {
+            item.style.display = estaConcluida ? 'flex' : 'none';
+        } else if (filtroSelecionado === 'pendente') {
+            item.style.display = !estaConcluida ? 'flex' : 'none';
+        }
+    });
+}
+
+function filtrarCategoria(filterSelect){
+
+    let lista = document.getElementById('lista');
+    lista.innerHTML = ''; 
+
+    if(filterSelect === 'Todas'){    
+        tarefas.forEach(novaTarefa => {
+            criarItem(novaTarefa);      
+        });
+    }else {
+        let filtradas = tarefas.filter(tarefa => tarefa.categorias === filterSelect);
+        filtradas.forEach(tarefa => criarItem(tarefa));
+        
+    }
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------//
+// Evento com funçõa de Excluir todas tarefas //
 let limpaTudo = document.getElementById('limpar');
 limpaTudo.addEventListener('click', function() {
     if (confirm('Tem certeza que deseja apagar todas as tarefas?')) {
